f67ad7f9cf935c576a750dfcbbd6db8275e26c61
Zarchyar <72223492+Zarchyar@users.noreply.github.com>
2024-01-14 20:48:48 -0500

    Update lootNuker.js
    instead of nuking the rice it now replaces the rice

--- kubejs/server_scripts/lootNuker.js
+++ kubejs/server_scripts/lootNuker.js
@@ -3,11 +3,16 @@
 onEvent("lootjs", (event) => {
     //event.enableLogging();
 	//console.info('loot nuke')
-    event
+    event//just delaing with rice dont mind me
+        .addBlockLootModifier("simplefarming:rice_crop")
+        .thenReplace('simplefarming:rice', 'farmersdelight:rice_panicle')
+        .thenRemove('simplefarming:rice_seeds');
+    
+    event//this part nukes the item from all loot tables
         .addLootTypeModifier(LootType.UNKNOWN, LootType.BLOCK, LootType.ENTITY, LootType.CHEST, LootType.FISHING, LootType.GIFT)
         .thenRemove("enigmaticlegacy:enigmatic_item")
         .thenRemove("enigmaticlegacy:loot_generator")
         .thenRemove("minecraft:spawner")
-        .thenRemove("simplefarming:rice")
-        .thenRemove("simplefarming:rice_seeds");
+        .thenReplace('simplefarming:rice', 'farmersdelight:rice_panicle')
+        .thenReplace('simplefarming:rice_seeds', 'farmersdelight:rice');
 });
