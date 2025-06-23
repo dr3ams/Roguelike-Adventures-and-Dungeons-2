7df22b5f80ac972080913c0afb0120596dd2696b
RecyclingTiger <89662661+RecyclingTiger@users.noreply.github.com>
2025-06-23 15:09:05 -0700

    Temporary fix to necromancer head lag issue
    Now removes the head, still briefly lags when it spawns but they won't lag the game after that

---
+++ config/paxi/datapacks/radrecipes/data/rad_recipes/functions/head_lag_fix.mcfunction
@@ -0,0 +1,1 @@
+execute if entity @s[nbt={ArmorItems:[{tag:{SkullOwner:"LightAseption"}}]}] run replaceitem entity @s armor.head minecraft:air
--- kubejs/server_scripts/mobtweaks.js
+++ kubejs/server_scripts/mobtweaks.js
@@ -12,10 +12,15 @@ onEvent('entity.spawned', event => {
 	if (event.entity.type == "blue_skies:summoner" || event.entity.type == "blue_skies:alchemist" || event.entity.type == "blue_skies:arachnarch" || event.entity.type == "blue_skies:starlit_crusher") {
 
     event.server.scheduleInTicks(1, event.server, (callback) => {
-      // spawning event is 1 tick before the mob actually spawns for some reason, this took me way too long to figure out
       callback.data.runCommandSilent(`execute as ${event.entity.id} unless entity @s[tag=has_been_nerfed] run function rad_recipes:blue_skies_boss_nerf`)
     })
 
+	}
+
+	if (event.entity.type == "minecraft:zombie") {
+    event.server.scheduleInTicks(1, event.server, (callback) => {
+      callback.data.runCommandSilent(`execute as ${event.entity.id} run function rad_recipes:head_lag_fix`)
+    })
 	}
 
 })
